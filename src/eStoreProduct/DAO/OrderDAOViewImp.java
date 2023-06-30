3e249aae5e3982c66ddc398a3df5e24a79fc5723
PotalaMeghana <135151485+PotalaMeghana@users.noreply.github.com>
2023-06-30 15:20:34 +0530

    Update OrderDAOViewImp.java

--- src/eStoreProduct/DAO/OrderDAOViewImp.java
+++ src/eStoreProduct/DAO/OrderDAOViewImp.java
@@ -11,6 +11,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import eStoreProduct.model.OrdersMapper;
 import eStoreProduct.model.OrdersViewModel;
 
@@ -19,6 +22,9 @@ public class OrderDAOViewImp implements OrderDAOView {
 
 	private final JdbcTemplate jdbcTemplate;
 	private final RowMapper<OrdersViewModel> ordersMapper;
+	private static final Logger logger = 
+			LoggerFactory.getLogger(customerOrderController.class);
+
 
 	public OrderDAOViewImp(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -27,6 +33,7 @@ public class OrderDAOViewImp implements OrderDAOView {
 
 	// Retrieve ordered products for a given customer ID
 	public List<OrdersViewModel> getorderProds(int custid) {
+		logger.info("eStoreProduct:OrderDAOViewImp:getting all the orders placed by the customer based on their id");
 		String query = "SELECT sp.prod_id, sp.prod_title, "
 				+ "sp.image_url, sp.prod_desc, "
 				+ "sps.prod_price, o.ordr_id, op.orpr_shipment_status "
@@ -41,6 +48,7 @@ public class OrderDAOViewImp implements OrderDAOView {
 
 	// Retrieve the details of a specific ordered product by customer ID and product ID
 	public OrdersViewModel OrdProductById(int c, Integer productId) {
+		logger.info("eStoreProduct:OrderDAOViewImp:getting the ordered product to display the product discription");
 		String query = "SELECT sp.prod_id, sp.prod_title, "
 				+ "sp.image_url, sp.prod_desc, "
 				+ "sps.prod_price, o.ordr_id, op.orpr_shipment_status "
@@ -55,12 +63,14 @@ public class OrderDAOViewImp implements OrderDAOView {
 
 	// Cancel an order by product ID and order ID
 	public void cancelorderbyId(Integer productId, int orderId) {
+		logger.info("eStoreProduct:OrderDAOViewImp:cancelling the order selected");
 		String updateQuery_products = "UPDATE slam_OrderProducts SET orpr_shipment_status = 'cancelled' WHERE prod_id = ? and ordr_id=?";
 		jdbcTemplate.update(updateQuery_products, productId, orderId);
 	}
 
 	// Get the shipment status of an order by product ID and order ID
 	public String getShipmentStatus(int productId, int orderId) {
+		logger.info("eStoreProduct:OrderDAOViewImp:getting the shipment status for tracking");
 		String sql = "SELECT orpr_shipment_status FROM slam_orderproducts WHERE prod_id = ? and ordr_id=?";
 
 		try {
@@ -72,6 +82,7 @@ public class OrderDAOViewImp implements OrderDAOView {
 
 	// Sort the list of ordered products by price based on the given sort order
 	public List<OrdersViewModel> sortProductsByPrice(List<OrdersViewModel> ordersList, String sortOrder) {
+		logger.info("eStoreProduct:OrderDAOViewImp:customer selected filter method for displaying the orders placed");
 		if (sortOrder.equals("lowToHigh")) {
 			Collections.sort(ordersList);
 		} else if (sortOrder.equals("highToLow")) {
@@ -83,6 +94,7 @@ public class OrderDAOViewImp implements OrderDAOView {
 	// Filter the list of ordered products by price range
 	public List<OrdersViewModel> filterProductsByPriceRange(List<OrdersViewModel> filteredProducts, double minPrice,
 			double maxPrice) {
+		logger.info("eStoreProduct:OrderDAOViewImp:method to display the ordered products based on the price range selected");
 		List<OrdersViewModel> res = new ArrayList<>();
 		for (OrdersViewModel product : filteredProducts) {
 			if (product.getPrice() >= minPrice && product.getPrice() <= maxPrice) {
@@ -94,6 +106,7 @@ public class OrderDAOViewImp implements OrderDAOView {
 
 	// Check if all products in an order are cancelled
 	public boolean areAllProductsCancelled(int orderId) {
+		logger.info("eStoreProduct:OrderDAOViewImp:getting all the cancelled orders");
 		String selectQuery = "SELECT COUNT(*) FROM slam_OrderProducts WHERE ordr_id = ? AND orpr_shipment_status != 'cancelled'";
 		int count = jdbcTemplate.queryForObject(selectQuery, Integer.class, orderId);
 		return count == 0;
@@ -101,6 +114,7 @@ public class OrderDAOViewImp implements OrderDAOView {
 
 	// Update the shipment status of an order
 	public void updateOrderShipmentStatus(int orderId, String shipmentStatus) {
+		logger.info("eStoreProduct:OrderDAOViewImp:updating the order shipment status by admin");
 		String updateQuery_orders = "UPDATE slam_Orders SET ordr_shipment_status = ? WHERE ordr_id = ?";
 		jdbcTemplate.update(updateQuery_orders, shipmentStatus, orderId);
 	}
