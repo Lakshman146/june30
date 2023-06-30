45257cafda415431905b44c275ffc1ceec058717
Lakshman <116259479+Lakshman146@users.noreply.github.com>
2023-06-30 15:25:41 +0530

    Update homeController.java

--- src/eStoreProduct/controller/homeController.java
+++ src/eStoreProduct/controller/homeController.java
@@ -8,7 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import eStoreProduct.DAO.cartDAO;
 import eStoreProduct.DAO.customerDAO;
 import eStoreProduct.model.custCredModel;
@@ -18,6 +19,7 @@ public class homeController {
 	static boolean flag = false;
 	customerDAO cdao;
 	cartDAO cd;
+	private static final Logger logger = LoggerFactory.getLogger(homeController.class);
 
 	// dependency injection
 	@Autowired
@@ -30,6 +32,7 @@ public class homeController {
 	// url mapping for home page
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getHomePage(Model model) {
+	logger.info("calling the home view page");
 		// call the view
 		return "home";
 	}
@@ -37,6 +40,7 @@ public class homeController {
 	// url mapping for logged in user to get back to home
 	@RequestMapping(value = "/loggedIn", method = RequestMethod.GET)
 	public String getHomeFoeLoggedUser(Model model) {
+	logger.info("after sign in getting the home view page");
 		// setting flag variable to maintain status and add to model
 		flag = true;
 		model.addAttribute("fl", flag);
@@ -47,6 +51,7 @@ public class homeController {
 	// url mapping to open signup page for new customer
 	@RequestMapping(value = "/signUp", method = RequestMethod.GET)
 	public String getSignUpPage(Model model) {
+		logger.info("calling the signup view page");
 		// call the view
 		return "signUp";
 	}
@@ -54,6 +59,7 @@ public class homeController {
 	// url mapping to open signin page
 	@RequestMapping(value = "/signIn", method = RequestMethod.GET)
 	public String getSignInPage(Model model) {
+	logger.info("calling the signin view page");
 		// call the view
 		return "signIn";
 	}
@@ -61,6 +67,7 @@ public class homeController {
 	// url mapping when customer completed the signup form
 	@RequestMapping(value = "/signInCreateAccount", method = RequestMethod.POST)
 	public String createAccount(@Validated custCredModel ccm, Model model) {
+	logger.info("Adding the new Customer to data base");
 		// add customer to database
 		boolean b = cdao.createCustomer(ccm);
 		// set it to the model
@@ -74,6 +81,7 @@ public class homeController {
 	// url mapping to redirect to home page by changing login status
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String userlogout(Model model, HttpSession session) {
+		logger.info("user logout");
 		// get the logged in session customer
 		// change the login status and add to model
 		flag = false;
