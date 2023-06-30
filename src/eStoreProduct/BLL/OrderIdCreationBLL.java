158ba3022b955b143e069662e3765305e4478bab
Lakshman <116259479+Lakshman146@users.noreply.github.com>
2023-06-30 15:42:11 +0530

    Update OrderIdCreationBLL.java

--- src/eStoreProduct/BLL/OrderIdCreationBLL.java
+++ src/eStoreProduct/BLL/OrderIdCreationBLL.java
@@ -2,14 +2,17 @@ package eStoreProduct.BLL;
 
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
 
 @Component
 public class OrderIdCreationBLL {
+		private static final Logger logger = LoggerFactory.getLogger(OrderIdCreationBLL.class);
 //create the orderId  
 	public String createRazorpayOrder(double amt) {
+				logger.info("eStoreProduct:OrderIdCreationBLL::creating the orderId");
 		System.out.println("amount in razorpay  " + amt);
 		long amountInPaise = (long) (amt * 100);
 		String orderId = null;
