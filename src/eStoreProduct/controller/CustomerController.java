967d83ef1774a11e41bf402fce208eb51f2ff372
Lakshman <116259479+Lakshman146@users.noreply.github.com>
2023-06-30 15:34:57 +0530

    Update CustomerController.java

--- src/eStoreProduct/controller/CustomerController.java
+++ src/eStoreProduct/controller/CustomerController.java
@@ -3,7 +3,8 @@ package eStoreProduct.controller;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
